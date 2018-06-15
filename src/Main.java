a2e059ed70a8e907817520392932d08d1591f93c
deviltt <tt416926063@qq.com>
2018-06-15 11:44:17 +0800

    A new beginning

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Hi, java");
+        System.out.println("I am very happy!");
     }
 }
