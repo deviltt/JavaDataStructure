b9851c8a845c8d9037d53168d05963fbe333853d
deviltt <tt416926063@qq.com>
2018-07-23 01:05:57 +0800

    前中序遍历递归非递归算法、后序遍历的递归算法

--- src/BST.java
+++ src/BST.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class BST<E extends Comparable<E>> {
     class Node{
         public E e;
@@ -30,7 +32,7 @@ public class BST<E extends Comparable<E>> {
         return size == 0;
     }
 
-    private void add(E e){
+    public void add(E e){
 //        if(root == null){
 //            root = new Node(e);
 //            size++; //每插入一个元素就要维护一下size
@@ -86,5 +88,106 @@ public class BST<E extends Comparable<E>> {
             return contains(node.left, e);
         else
             return contains(node.right, e);
+    }
+
+    //二分搜索树的前序遍历
+    public void preOrder(){
+        preOrder(root);
+    }
+
+    public void preOrder(Node node){
+        if(node == null)
+            return;
+
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    //前序遍历的非递归实现
+    public void preOrderNR(){
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            Node cur = stack.pop();
+            System.out.println(cur.e);
+
+            if(cur.right != null)
+                stack.push(cur.right);
+            if(cur.left != null)
+                stack.push(cur.left);
+
+        }
+    }
+
+    public void inOrder(){
+        inOrder(root);
+    }
+
+    private void inOrder(Node node){
+        if(node == null)
+            return;
+
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+
+    //中序遍历的非递归实现
+    //将T入栈，然后访问T的左子树，访问完T的左子树后将T出栈访问T，然后访问T的右子树
+    public void inOrderNR(){
+        Stack<Node> stack = new Stack<>();
+        Node node = root;
+
+        while(node != null || !stack.isEmpty()){
+            if(node != null){
+                stack.push(node);
+                node = node.left;
+            }
+            else{
+                Node node1 = stack.peek();
+                System.out.println(node1.e);
+                stack.pop();            //访问完中间节点后就出栈，但在出栈之前还要保留，因为还要访问右节点
+                node = node1.right;
+            }
+        }
+    }
+
+    public void postOrder(){
+        postOrder(root);
+    }
+
+    private void postOrder(Node node){
+        if(node == null)
+            return;
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.e);
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        generateBSTString(root, 0, res);
+        return res.toString();
+    }
+
+    //生成以node为根节点，深度为depth的描述二叉树的字符串
+    private void generateBSTString(Node node, int depth, StringBuilder res){
+        if(node == null){
+            res.append(generateDepthString(depth) + "null\n");
+            return;
+        }
+
+        res.append(generateDepthString(depth) + node.e + "\n");
+        generateBSTString(node.left, depth + 1, res);
+        generateBSTString(node.right, depth + 1, res);
+    }
+
+    private String generateDepthString(int depth){
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < depth; i++)
+            res.append("--");
+        return res.toString();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,23 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("I am very happy!");
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+
+        for(int num : nums)
+            bst.add(num);
+
+//        bst.preOrder();
+//
+//        System.out.println();
+//
+//        bst.preOrderNR();
+//        System.out.println();
+        bst.inOrderNR();
+//        System.out.println(bst);
+//
+//        bst.inOrder();
+//        System.out.println();
+//        bst.postOrder();
     }
 }
