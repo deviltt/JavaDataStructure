8f174bfecd50416f2870d5f1c97f959421663cd2
deviltt <tt416926063@qq.com>
2018-09-09 23:16:20 +0800

    三种方法逆转单链表

--- src/leetcode/Solution206.java
+++ src/leetcode/Solution206.java
@@ -8,14 +8,32 @@ package leetcode;
 public class Solution206 {
     public ListNode reverseList(ListNode head) {
         //头插法
-        ListNode dummyHead = new ListNode(0);
-        dummyHead.next = null;
-        ListNode p = head, q = head;
-        while(p != null){
-            p = q.next;
-            q.next = dummyHead.next;
-            dummyHead.next = q;
-            q = p;
+//        ListNode dummyHead = new ListNode(0);
+//        dummyHead.next = null;
+//        ListNode p = head, q = head;
+//        while(p != null){
+//            p = q.next;
+//            q.next = dummyHead.next;
+//            dummyHead.next = q;
+//            q = p;
+//        }
+//        return dummyHead.next;
+        //递归法
+//        if(head == null || head.next == null)
+//            return head;
+//        ListNode p = reverseList(head.next);
+//        head.next.next = head;
+//        head.next = null;
+//        return p;
+        //原地逆转法
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode prec = head;
+        while(prec.next != null){
+            ListNode c = prec.next;
+            prec = c.next;
+            c.next = dummyHead.next;
+            dummyHead.next = c;
         }
         return dummyHead.next;
     }
