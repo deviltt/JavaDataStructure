0820ec43f246079307ada2460623c757a3040a29
deviltt <tt416926063@qq.com>
2018-11-18 12:39:30 +0800

    Comparable可以认为是一个内比较器，实现了Comparable接口的类有一个特点，就是这些类是可以和自己比较的，至于具体和另一个实现了Comparable接口的类如何比较，则依赖compareTo方法的实现，compareTo方法也被称为自然比较方法。

---
+++ src/comparable/Person1.java
@@ -0,0 +1,63 @@
+/**
+ * 功能：对类进行排序，先按照年龄排序，年龄相同则按照分数排序
+ */
+package comparable;
+
+import java.util.Arrays;
+
+public class Person1 implements Comparable<Person1>{
+    private int age;
+    private int score;
+
+    public Person1(int age, int score){
+        this.age = age;
+        this.score = score;
+    }
+
+    @Override
+    public int compareTo(Person1 o) {
+//        if(this.age > o.age)
+//            return 1;
+//        else if(this.age < o.age)
+//            return -1;
+//        else {
+//            if(this.score > o.score)
+//                return 1;
+//            else if(this.score < o.score)
+//                return -1;
+//            else
+//                return 0;
+//        }
+        if(Integer.compare(this.age, o.age) > 0)
+            return 1;
+        else if(Integer.compare(this.age, o.age) < 0)
+            return -1;
+        else {
+            if(Integer.compare(this.score, o.score) > 0)
+                return 1;
+            else if(Integer.compare(this.score, o.score) < 0)
+                return -1;
+            else
+                return 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "age: " + age + " score: " + score;
+    }
+
+    public static void main(String[] args){
+        Person1[] p = new Person1[]{new Person1(22, 39),
+                new Person1(25, 54),
+                new Person1(22, 32),
+                new Person1(14, 34),
+                new Person1(25, 32)
+        };
+
+        Arrays.sort(p);
+
+        for(Person1 pp : p)
+            System.out.println(pp.toString());
+    }
+}
