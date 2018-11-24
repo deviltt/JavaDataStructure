5af00644ea41a08a278835bf3ec0ce45bb0f43d0
deviltt <tt416926063@qq.com>
2018-11-24 14:03:25 +0800

    希尔排序注释了一下

--- src/sort/ShellSort.java
+++ src/sort/ShellSort.java
@@ -2,6 +2,7 @@ package sort;
 
 public class ShellSort {
     public void shellSort(int[] arr){
+        //确定步长
         for(int gap = arr.length / 2; gap > 0; gap /= 2){
             for(int i = gap; i < arr.length; i++){
                 int j = i;
